dd6497fdc9be1c53a2af07abfe05e58e32e4f6de
abhishekpatel2004 <abishekpatel46@gmail.com>
2025-05-06 10:06:49 +0545

    watrer reminder

---
+++ lib/service/water_api_service.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/water_intake_model.dart';
+
+class WaterApiService {
+  final String baseUrl;
+
+  WaterApiService(this.baseUrl);
+
+  Future<WaterIntakeModel?> fetchDailyIntake(String userId) async {
+    final url = Uri.parse('$baseUrl/api/water/daily?userId=$userId');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return WaterIntakeModel.fromJson(json.decode(response.body));
+    }
+    return null;
+  }
+
+  Future<bool> addIntake(String userId, double liters) async {
+    final url = Uri.parse('$baseUrl/api/water/add');
+    final response = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: json.encode({"userId": userId, "amountLiters": liters}),
+    );
+    return response.statusCode == 201;
+  }
+}
